9a9e8c96ead085d47dd605d29b689fa2d2bd526e
brendanshen24 <91840687+brendanshen24@users.noreply.github.com>
2026-03-18 21:12:08 -0700

    old teaser site

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,30 @@
-<h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+</script>
+
+<div class="bg-[#F2CB25] shadow-[0_4px_4px_0_rgba(0,0,0,0.25)] h-screen w-full min-w-screen flex flex-col justify-between items-stretch m-0 overflow-hidden relative p-10">
+
+    <div class="flex-1 flex justify-center items-center">
+        <div class="relative w-auto">
+            <img src="/sj_logo.svg" alt="SparkJam Logo" class="w-full h-auto" />
+            <img src="/star.svg" alt="Star" class="absolute top-[12%] -right-[4%] w-[12%] animate-spin [animation-duration:2s]" />
+        </div>
+    </div>
+
+    <div class="flex flex-row justify-between items-end gap-0 w-full">
+        <div class="text-[#000] font-['PPMori'] text-[16px] font-semibold leading-[18px] tracking-[-0.32px]">
+            <h6>More info</h6>
+            <h6>April 2nd</h6>
+        </div>
+
+        <div class="text-[#000] font-['PPMori'] text-[16px] font-semibold leading-[18px] tracking-[-0.32px] flex flex-row gap-2 md:gap-[20px]">
+            <div class="flex flex-col">
+                <h6>Learning Week</h6>
+                <h6>May 11 - 15</h6>
+            </div>
+            <div>
+                <h6>Designing Week</h6>
+                <h6>May 16 - 23</h6>
+            </div>
+        </div>
+    </div>
+</div>
