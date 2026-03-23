2e98862206c68cd14f18933ac397c46fc4f73235
brendanshen24 <91840687+brendanshen24@users.noreply.github.com>
2026-03-23 13:50:54 -0700

    updated sponsors section

--- src/lib/components/home/SponsorsSection.svelte
+++ src/lib/components/home/SponsorsSection.svelte
@@ -5,21 +5,41 @@
 	</p>
 	<div class="mt-16 flex flex-col items-center gap-16">
 		<div class="flex flex-col items-center gap-10">
-			<img src="/sponsors/transoft.png" alt="Sponsor" class="max-h-[190px] w-auto max-w-full object-contain" />
-			<img src="/sponsors/venturelabs.png" alt="Sponsor" class="max-h-[76px] w-full max-w-md object-contain" />
+			<a href="https://www.transoftsolutions.com/" target="_blank" rel="noopener noreferrer" class="transition-opacity hover:opacity-80">
+				<img src="/sponsors/transoft.png" alt="Transoft Solutions" class="max-h-[190px] w-auto max-w-full object-contain" />
+			</a>
+			<a href="https://venturelabs.ca/" target="_blank" rel="noopener noreferrer" class="transition-opacity hover:opacity-80">
+				<img src="/sponsors/venturelabs.png" alt="SFU VentureLabs" class="max-h-[76px] w-full max-w-md object-contain" />
+			</a>
 		</div>
 		<div class="flex w-full flex-col gap-8">
 			<div class="flex flex-wrap items-center justify-center gap-12">
-				<img src="/sponsors/vercel.png" alt="" class="h-10 object-contain" />
-				<img src="/sponsors/framer.png" alt="" class="h-12 object-contain" />
-				<img src="/sponsors/github.png" alt="" class="h-14 object-contain" />
+				<a href="https://vercel.com/" target="_blank" rel="noopener noreferrer" class="transition-opacity hover:opacity-80">
+					<img src="/sponsors/vercel.png" alt="Vercel" class="h-10 object-contain" />
+				</a>
+				<a href="https://www.framer.com/" target="_blank" rel="noopener noreferrer" class="transition-opacity hover:opacity-80">
+					<img src="/sponsors/framer.png" alt="Framer" class="h-12 object-contain" />
+				</a>
+				<a href="https://github.com/" target="_blank" rel="noopener noreferrer" class="transition-opacity hover:opacity-80">
+					<img src="/sponsors/github.png" alt="GitHub" class="h-14 object-contain" />
+				</a>
 			</div>
 			<div class="flex flex-wrap items-center justify-between gap-8">
-				<img src="/sponsors/huion.png" alt="" class="h-[79px] object-contain" />
-				<img src="/sponsors/hanoidrip.png" alt="" class="h-[79px] rounded-xl object-contain" />
-				<img src="/sponsors/hive.png" alt="" class="h-[68px] object-contain" />
-				<img src="/sponsors/indomie.png" alt="" class="h-[79px] object-contain" />
-				<img src="/sponsors/tacofino.png" alt="" class="h-[41px] object-contain" />
+				<a href="https://www.huion.com/" target="_blank" rel="noopener noreferrer" class="transition-opacity hover:opacity-80">
+					<img src="/sponsors/huion.png" alt="Huion" class="h-[79px] object-contain" />
+				</a>
+				<a href="https://www.instagram.com/hanoidrip/" target="_blank" rel="noopener noreferrer" class="transition-opacity hover:opacity-80">
+					<img src="/sponsors/hanoidrip.png" alt="Hanoi Drip" class="h-[79px] rounded-xl object-contain" />
+				</a>
+				<a href="https://www.hiveclimbing.com/" target="_blank" rel="noopener noreferrer" class="transition-opacity hover:opacity-80">
+					<img src="/sponsors/hive.png" alt="Hive Cafe" class="h-[68px] object-contain" />
+				</a>
+				<a href="https://www.indomie.com/" target="_blank" rel="noopener noreferrer" class="transition-opacity hover:opacity-80">
+					<img src="/sponsors/indomie.png" alt="Indomie" class="h-[79px] object-contain" />
+				</a>
+				<a href="https://www.tacofino.com/" target="_blank" rel="noopener noreferrer" class="transition-opacity hover:opacity-80">
+					<img src="/sponsors/tacofino.png" alt="Tacofino" class="h-[41px] object-contain" />
+				</a>
 			</div>
 		</div>
 	</div>
