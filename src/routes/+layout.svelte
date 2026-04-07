a97668eb4cff4b754a0980a86980c14aea477bb7
brendanshen24 <91840687+brendanshen24@users.noreply.github.com>
2026-04-06 19:31:07 -0700

    brought back old cursors and imported new footer shapes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,6 @@
 <script lang="ts">
 	import './layout.css';
-	import CustomCursor from '$lib/components/CustomCursor.svelte';
-
 	const { children } = $props();
 </script>
 
-<CustomCursor />
 {@render children()}
